a18911abe12ec49b6ad230fd8b983daac80eaa69
Accelerator <2338781342@qq.com>
2021-01-17 16:42:39 +0800

    1.17update
    照着敲出来的3d移动和跟踪摄像机脚本

---
+++ moving scripts/Moving_Sphere3.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+
+public class Moving_Sphere3 : MonoBehaviour
+{
+
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;//最多跳几次
+
+    [SerializeField, Range(0, 5)]
+    float maxGroundAngle = 25f;
+    Vector3 contactNormal;//跳跃垂直与当前地面法线
+    Vector3 velocity, desiedVelocity;
+    Rigidbody body;
+    bool desiredJump, onGround;
+    int jumpPhase;//当前已跳跃次数
+    float minGroundDotProduct;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        OnValidate();
+    }
+    void EvaluateCollision(Collision col)
+    {
+        for (int i = 0; i < col.contactCount; i++)
+        {
+            //遍历某次碰撞的所有接触点,有偶数个点
+            //y值大于设定的角度->在地面（仅限此处球体）
+            //即角度小于min的情况下，算做地面
+            Vector3 normal = col.GetContact(i).normal;
+            if(normal.y >= minGroundDotProduct)
+            {
+                onGround = true;
+                contactNormal = normal;
+            }
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        EvaluateCollision(col);
+    }
+    void OnCollisionStay(Collision col)//即不接触任何物体判断为不在地
+    {
+        EvaluateCollision(col);
+    }
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        desiedVelocity = new Vector3(
+            playerInput.x, 0f, playerInput.y) * maxSpeed;//期望速度
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+    void FixedUpdate()
+    {
+        UpdateState();
+        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiedVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiedVelocity.z, maxSpeedChange);
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+        body.velocity = velocity;
+        onGround = false;
+    }
+    void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle*Mathf.Deg2Rad);
+    }
+    void UpdateState()
+    {
+        velocity = body.velocity;
+        if (onGround) jumpPhase = 0;//一旦落地，跳跃归零
+        else
+        {
+            contactNormal = Vector3.up;
+
+        }
+    }
+    void Jump()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+            // 这个情况只会出现在多段跳里,jumpspeed将要添加的速度
+            //要控制多段跳的速度不能大于单次跳的速度
+            //此时判断velocity.y > 0f不再正确,要判断地面法线方向上的速度
+            if(alignedSpeed > 0f)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+            }
+            velocity+= contactNormal *jumpSpeed;
+        }
+
+    }
+    Vector3 ProjectOnContactPlane(Vector3 vector)
+    {
+        //输入向量，返回此向量在接触面的投影
+        return vector = contactNormal * Vector3.Dot(vector, contactNormal);
+    }
+    void AdjustVelocity()
+    {//相对接触面进行速度修改
+        Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+        Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+        float currentX = Vector3.Dot(velocity, xAxis);
+        float currentZ = Vector3.Dot(velocity, zAxis);
+
+        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiedVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiedVelocity.z, maxSpeedChange);
+            
+    }
+}
+
+
---
+++ moving scripts/Orbit_Camera3.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit_Camera3 : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform focus = default;
+
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -45f, maxVerticalAngle = 45f;
+
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;
+
+    [SerializeField, Range(0f, 90f)]
+    float alignSmoothRange = 45f;
+
+    [SerializeField]
+    LayerMask obstructionMask = -1;
+
+    Camera regularCamera;
+
+    Vector3 focusPoint, previousFocusPoint;
+
+    Vector2 orbitAngles = new Vector2(45f, 0f);
+
+    float lastManualRotationTime;
+
+    Vector3 CameraHalfExtends
+    {
+        get
+        {
+            Vector3 halfExtends;
+            halfExtends.y =
+                regularCamera.nearClipPlane *
+                Mathf.Tan(0.5f * Mathf.Deg2Rad * regularCamera.fieldOfView);
+            halfExtends.x = halfExtends.y * regularCamera.aspect;
+            halfExtends.z = 0f;
+            return halfExtends;
+        }
+    }
+    void Awake()
+    {
+        regularCamera = GetComponent<Camera>();
+        focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+    }
+    void UpdateFoucsPoint()
+    {
+        //主要作用：求focusPonit，记录previousFocusPoint
+        previousFocusPoint = focusPoint;
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f)
+        {
+            float distance2 = Vector3.Distance(targetPoint, focusPoint);
+            float t = 1f;
+            if (distance2 > 0.01f && focusCentering > 0f)
+            {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+            if (distance2 > focusRadius)
+            {
+                t = Mathf.Min(t, focusRadius / distance2);
+            }
+            focusPoint = Vector3.Lerp(
+                    targetPoint, focusPoint, t);
+        }
+        else focusPoint = targetPoint;
+    }
+    int cnt = 0;
+    bool ManualRotation()
+    {//返回值：根据鼠标坐标输入，是否需要更改相机转角
+     /*Vector2 input = new Vector2(
+         Input.GetAxis("Vertical Camera"),
+         Input.GetAxis("Horizontal Camera"));*/
+        Vector2 input = new Vector2(0,0);
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            //Debug.Log((cnt++)+" "+input.x);
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+        return false;
+    }
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+            maxVerticalAngle = minVerticalAngle;
+    }
+    bool AutomaticRotation()
+    {
+        //Debug.Log("Now Time : "+(lastManualRotationTime ));
+        if (Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+        Vector2 movement = new Vector2(
+            focusPoint.x - previousFocusPoint.x,
+            focusPoint.z - previousFocusPoint.z);
+        float movementDeltaSqr = movement.sqrMagnitude;
+        if (movementDeltaSqr < 0.0001f)
+        {
+            return false;//如果当前角度和上次处理时没变化，那就不需要改变转角
+        }
+        //否则把orbitAngles改成 由相差角度 得到的 向量headingAngle
+        float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDeltaSqr));
+        float rotationChange = rotationSpeed * Mathf.Min(
+            movementDeltaSqr, Time.unscaledDeltaTime);
+        //平缓变换，当前需要转多少角度?
+        //变换速度不应保持线性，加入随剩余角度变化的速度
+        //当剩余角小于alignSmoothRange时，乘上对应比例
+        float deltaAbs = Mathf.Abs(Mathf.DeltaAngle(orbitAngles.y, headingAngle));
+        if (deltaAbs < alignSmoothRange)
+            rotationChange *= deltaAbs / alignSmoothRange;
+        else if (180f - deltaAbs < alignSmoothRange)
+            rotationChange *= (180f - deltaAbs) / alignSmoothRange;
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
+        return true;
+    }
+    void ConstrainAngles()
+    {
+        orbitAngles.x =
+            Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+        if (orbitAngles.y < 0f) orbitAngles.y += 360f;
+        else if (orbitAngles.y >= 360f) orbitAngles.y -= 360f;
+    }
+    void LateUpdate()
+    {
+
+        UpdateFoucsPoint();
+        Quaternion lookRotation = transform.localRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+            //相机需要转变角度，则全局orbitAngles 控制当前转角
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+        //摄像机遮挡检测，相机 -> 物体被挡住后，相机位置改为 阻挡点->物体
+        RaycastHit hit;
+        /*if (Physics.Raycast(
+            focusPoint ,- lookDirection , out hit , distance))
+        {
+            lookPosition = focusPoint - lookDirection * hit.distance;
+        }*/
+        if (Physics.BoxCast(focusPoint, CameraHalfExtends
+            ,-lookDirection, out hit,lookRotation, distance))
+        {
+            lookPosition = focusPoint - lookDirection * (hit.distance +regularCamera.nearClipPlane);
+        }
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+    static float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        return direction.x < 0f ? 360f - angle : angle;
+    }
+
+}
